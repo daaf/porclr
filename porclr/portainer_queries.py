6810fd415fbe3988e5e8b0109c4e0204223752f7
Derek Farr <dfarr3@gmail.com>
2022-04-03 21:30:38 -0400

    clean up portainer_queries.py

--- porclr/portainer_queries.py
+++ porclr/portainer_queries.py
@@ -22,7 +22,6 @@ def get_stack_list(url: str, auth_token: str):
     response = requests.get(f"http://{url}/api/stacks", headers=headers)
     response.raise_for_status()
     data = json.loads(response.text)
-    print(data)
     stack_list = [{"id": stack["Id"], "name": stack["Name"]} for stack in data]
 
     return stack_list
@@ -35,15 +34,3 @@ def get_compose_file(url: str, stack_id: str, auth_token: str):
     data = json.loads(response.text)
     compose_file = data["StackFileContent"]
     return compose_file
-
-
-# curl \
-# -X POST http://127.0.0.1:9443/api/auth \
-# -H "Content-Type: application/json" \
-# -d '{"username": "admin", "password":"Practicedaily215!"}'
-
-
-# curl \
-#   -H "Authorization: Bearer \
-#   eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbiIsInJvbGUiOjEsInNjb3BlIjoiZGVmYXVsdCIsImV4cCI6MTY0ODkzNTQ0OX0.M5ZMHkTmDaNZguMc8MFnROrLSX2AFlA55OHB5tABChE" \
-#   http://127.0.0.1:9443/api/stacks
