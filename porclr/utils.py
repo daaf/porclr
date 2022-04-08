22917d949bba1bbc8bb4682354861b152bc2a784
Derek Farr <dfarr3@gmail.com>
2022-04-08 17:44:57 -0400

    add docstring to utils.py

--- porclr/utils.py
+++ porclr/utils.py
@@ -8,6 +8,12 @@ from os import path, mkdir
 
 
 def create_dir_if_not_extant(path_to_dir: str) -> None:
+    """
+    Creates a directory at the specfied path if it doesn't already exist.
+
+    :param path_to_dir: The path to the directory that should be created if it
+        doesn't already exist.
+    """
     echo(f"\nChecking for directory {path_to_dir}...")
 
     if not path.exists(path_to_dir):
